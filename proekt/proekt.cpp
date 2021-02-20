6252398af84a2d4ecb2640d7be6f48adec9b96ed
BartOstrovv <tapasz1488@gmail.com>
2021-02-20 23:15:30 +0200

    Func Save in cBasket and Load in cRestoran

--- proekt/proekt.cpp
+++ proekt/proekt.cpp
@@ -9,13 +9,15 @@ using namespace std;
 struct Dish
 {
 	string m_Name;
+	string m_About;
 	string m_Weight;
 	string m_Price;
 	int count = 1;
+	Dish() {};
 	Dish(string n, string w, string p) :m_Name(n), m_Weight(w), m_Price(p) {};
 	void Show()
 	{
-		cout << "Name: " << m_Name << endl;
+		cout << "Name: " <<m_Name << " -- " << m_About << endl;
 		cout << "Weight: " << m_Weight << endl;
 		cout << "Price: " << m_Price << endl;
 		cout << "==============\n";
@@ -29,8 +31,28 @@ class Restoran
 	string m_RaitRest;
 public:
 	Restoran(){}
-	Restoran(vector<Dish> v, string name, string street, string rait) :
-		m_NameRest(name), m_StreetRest(street), m_RaitRest(rait), m_Menu(v) {}
+	Restoran(string name, string street, string rait, string namefileMenu) :
+		m_NameRest(name), m_StreetRest(street), m_RaitRest(rait) 
+	{
+		ifstream inFile(namefileMenu, ifstream::in);
+		Dish temp;
+		try
+		{
+			if (!inFile.is_open()) throw 0;
+			while (!inFile.eof())
+			{
+				getline(inFile, temp.m_Name);
+				getline(inFile, temp.m_About);
+				getline(inFile, temp.m_Weight);
+				getline(inFile, temp.m_Price);
+				m_Menu.push_back(temp);
+		   }
+		}
+		catch (exception& ex)
+		{
+			cout << "Error open file: " << ex.what() << endl;
+		}
+	}
 	void ShowInfoRest()
 	{
 		cout << "Name restoran: " << m_NameRest << endl;
@@ -58,6 +80,24 @@ public:
 		for (auto &i : PokypkaBlya) i.Show();
 		cout << "\t";
 	}
+	bool SaveCheck(string namefile)
+	{
+		ofstream outFile(namefile, ofstream::out);
+		try
+		{
+			if (!outFile.is_open()) throw 0;
+			for_each(PokypkaBlya.begin(), PokypkaBlya.end(),
+				[&](Dish vd) {
+					outFile << "Name: " << vd.m_Name << " | ";
+					outFile << "Price: " << vd.m_Price << endl;
+				});
+		}
+		catch (exception& ex)
+		{
+			cout << "Error: " << ex.what() << endl;
+		}
+		outFile.close();
+	}
 };
 
 
@@ -65,12 +105,11 @@ int main()
 {
 
 	Basket bas;
-	bas.AddElement(Dish("salat", "122", "113"));
-	Restoran buk(vector<Dish>{Dish("salawwt", "122", "113"), Dish("salat", "122", "113"), Dish("salat", "122", "113")},"Restik","Brodrr","13,6");
-	Restoran buk2(vector<Dish>{Dish("sawlat", "122", "113"), Dish("salat", "122", "113"), Dish("salat", "122", "113")}, "Restik", "Brodrr", "13,6");
-	Restoran buk3(vector<Dish>{Dish("swddwalat", "122", "113"), Dish("salat", "122", "113"), Dish("salat", "122", "113")}, "aaaaaaaastik", "Baaarr", "13,6");
-	vector<Restoran> r{ buk,buk2,buk3 };
-	for (auto& i : r)
-		i.ShowInfoRest();
-
+	bas.AddElement(Dish("Cesar", "122", "113"));
+	bas.AddElement(Dish("Cesar", "12", "200"));
+	bas.AddElement(Dish("Shuba", "122", "113"));
+	bas.AddElement(Dish("Olive", "122", "113"));
+	bas.SaveCheck("testSaveCheck.txt");
+	Restoran buk("Pobeda", "Stysa 2", "9/10", "LoadMenuToRestoran.txt");
+	buk.ShowInfoRest();
 }
